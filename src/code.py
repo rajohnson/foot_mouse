820d823e4691d1bdbbaf0b2d9828a3f564de5a3c
Rob Johnson <Johnson.rob.alan@gmail.com>
2023-01-21 13:56:11 -0800

    add encoder and tie rotation to scrolling for the mouse.

--- src/code.py
+++ src/code.py
@@ -3,6 +3,7 @@ from digitalio import DigitalInOut, Direction, Pull
 import usb_hid
 from adafruit_hid.mouse import Mouse
 from adafruit_debouncer import Debouncer
+import rotaryio
 
 led = DigitalInOut(board.LED)
 led.direction = Direction.OUTPUT
@@ -11,6 +12,9 @@ switch_pin = DigitalInOut(board.GP0)
 switch_pin.direction = Direction.INPUT
 switch_pin.pull = Pull.UP
 switch = Debouncer(switch_pin)
+
+encoder = rotaryio.IncrementalEncoder(board.GP2, board.GP3)
+encoder_last = encoder.position
 
 mouse = Mouse(usb_hid.devices)
 
@@ -22,3 +26,8 @@ while True:
         mouse.release(Mouse.LEFT_BUTTON)
     else:
         mouse.press(Mouse.LEFT_BUTTON)
+
+    if encoder.position != encoder_last:
+        delta = encoder_last - encoder.position
+        encoder_last = encoder.position
+        mouse.move(wheel=delta)
