6b4a1266aebc3c8345e6aa92ba8a9f376319ce36
Rob Johnson <Johnson.rob.alan@gmail.com>
2023-01-21 12:49:54 -0800

    click mouse when button is pushed.

--- src/code.py
+++ src/code.py
@@ -1,6 +1,7 @@
-import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
+import usb_hid
+from adafruit_hid.mouse import Mouse
 from adafruit_debouncer import Debouncer
 
 led = DigitalInOut(board.LED)
@@ -11,7 +12,13 @@ switch_pin.direction = Direction.INPUT
 switch_pin.pull = Pull.UP
 switch = Debouncer(switch_pin)
 
+mouse = Mouse(usb_hid.devices)
+
 while True:
     switch.update()
     led.value = switch.value
-    switch.update()
+
+    if switch.value:
+        mouse.press(Mouse.LEFT_BUTTON)
+    else:
+        mouse.release(Mouse.LEFT_BUTTON)
