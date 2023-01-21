86f1cecf53c96f7d694bf0cabfd2e19ae988e463
Rob Johnson <Johnson.rob.alan@gmail.com>
2023-01-21 12:46:37 -0800

    debounce the switch

--- src/code.py
+++ src/code.py
@@ -1,13 +1,17 @@
 import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
+from adafruit_debouncer import Debouncer
 
 led = DigitalInOut(board.LED)
 led.direction = Direction.OUTPUT
 
-switch = DigitalInOut(board.GP0)
-switch.direction = Direction.INPUT
-switch.pull = Pull.UP
+switch_pin = DigitalInOut(board.GP0)
+switch_pin.direction = Direction.INPUT
+switch_pin.pull = Pull.UP
+switch = Debouncer(switch_pin)
 
 while True:
+    switch.update()
     led.value = switch.value
+    switch.update()
